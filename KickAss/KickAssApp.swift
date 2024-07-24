ebbc5f700bbf70b9102c1fd0fce9a7395e465e26
DRSaz <schmidliaz@gmail.com>
2024-07-24 07:59:42 -0700

    Initial Commit

---
+++ KickAss/KickAssApp.swift
@@ -0,0 +1,17 @@
+//
+//  KickAssApp.swift
+//  KickAss
+//
+//  Created by Infinite Novice on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct KickAssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
